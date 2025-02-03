548279f2410367f2ed21f1ce3c49321d44a11f63
rashimishra9695 <rashimishra9695@gmail.com>
2025-02-03 12:41:11 +0530

    register and login page UI

--- client/src/screens/Loginscreen.js
+++ client/src/screens/Loginscreen.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Loginscreen() {
-    return (
+    const [email , setemail] = useState('')
+    const [password , setpassword] = useState('')
+    function Login(){
+        const user = {
+            email,
+            password
+        }
+    }
+    return ( <div className='login'>
+        <div className="row justify-content-center mt-1">
+          <div className="col-md-3 mt-3 text-left shadow-lg p-3 mb-5 bg-white rounded">
+          <h2 className="text-center m-2" style={{ fontSize: "35px" }}>
+            Login
+          </h2>
+  
         <div>
-           <h1>Login Screen</h1> 
-        </div>
-    )
+            <input required type="text" placeholder="email" className="form-control mt-1"  value={email} onChange={(e)=>{setemail(e.target.value)}} />
+            <input
+              type="password"
+              placeholder="password"
+              className="form-control mt-1"  
+              value={password} required onChange={(e)=>{setpassword(e.target.value)}}         
+            />
+            
+            <button className="btn btn-success mt-3 mb-3 rounded-pill">LOGIN</button>
+            <br/>
+            <a style={{color:'black'}} href="/register" className="mt-2">Click Here To Register</a>
+          </div>
+    
+    </div>
+    </div>
+    </div>   )
 }
 
 export default Loginscreen
--- client/src/screens/Registerscreen.js
+++ client/src/screens/Registerscreen.js
@@ -1,13 +1,62 @@
 import React from 'react'
+import { useState} from 'react';
 
-function Register () {
+function RegisterScreen () {
+    const [name , setname] = useState('')
+    const [email, setemail] = useState('')
+    const [password, setpassword] = useState('')
+    const [cpassword, setcpassword] = useState('')
+
+    function register(){
+        if(password==cpassword){
+        const user={
+            name,
+            email,
+            password,
+            cpassword
+        }
+        console.log(user);
+    }
+    else{
+        alert('Password not matched')
+    } 
+    }
     return (
-        <div>
-            <h1>
-                Register Screen
-            </h1>
+        <div className='register'>
+      <div className="row justify-content-center mt-5">
+        <div className="col-md-3 mt-3 text-left shadow-lg p-3 mb-5 bg-white rounded">
+
+      
+          <h2 className="text-center m-2" style={{ fontSize: "35px" }}>
+            Register
+          </h2>
+          <div>
+            <input required type="text" placeholder="name" className="form-control mt-1"  value={name} onChange={(e)=>{setname(e.target.value)}}/>
+            <input required type="text" placeholder="email" className="form-control mt-1" value={email} onChange={(e)=>{setemail(e.target.value)}} />
+            <input required type="phone" placeholder="phone" className="form-control mt-1"  />
+            <input required type="text" placeholder="location" className="form-control mt-1"  /> <input
+              type="password"
+              placeholder="password"
+              className="form-control mt-1"
+              value={password} onChange={(e)=>{setpassword(e.target.value)}}
+              
+            />
+            <input
+              type="text"
+              placeholder="confirm password"
+              className="form-control mt-1"
+              value={cpassword} onChange={(e)=>{setcpassword(e.target.value)}}
+              
+            />
+           
+            <button className="btn btn-primary rounded-pill mt-3 mb-3" onClick={register}>REGISTER</button>
+            <br/>
+            <a style={{color:'black'}} href="/login">Click Here To Login</a>
+          </div>
         </div>
+      </div>
+    </div>
     )
 }
 
-export default Register
+export default RegisterScreen
